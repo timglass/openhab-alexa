ae79ea5298696968c216d9f79a717354d99cadce
Tim Glass <tim@timglass.co.uk>
2018-02-01 23:28:38 +0000

    Splitting out routes from index.js

--- openhab-alexa/src/index.js
+++ openhab-alexa/src/index.js
@@ -2,47 +2,21 @@ import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import { log } from 'util';
-import axios from 'axios';
+
+import router from './routes'
 
 let PORT = 8080;
 var config = { port: null };
 
-let httpClient = axios.create({
-    headers: {"Content-Type": "text/plain",  "Accept": "application/json"}
-})
+
 let app = express();
 app.server = http.createServer(app);
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (request, response) => {
-    response.send('Hello GET from Express!')
-})
-
-app.post('/', (request, response) => {
-    console.log(request.body.amount)
-    var alexaRequest = request.body
-    var dimmerPercentage = 0
-    if (alexaRequest.amount === "ON") {
-        dimmerPercentage = 100;
-    } else if (alexaRequest.amount === "OFF") {
-        dimmerPercentage = 0;
-    } else {
-        dimmerPercentage = alexaRequest.amount;
-    }
-    const openhabReq = {
-        url: "http://openhab:8080/rest/items/Kitchen_Light_Dimmer",
-        //url: "http://openhab:8080" + "/rest/items/" + alexaRequest.item,
-        payload: dimmerPercentage
-    }
-
-    const logMessage = "Sending HTTP GET with body '"+openhabReq.payload+"' to " + openhabReq.url
-    httpClient.post(openhabReq.url, openhabReq.payload)
-         .then(response => console.log(response))
-         .catch(err => console.log(err));
-    response.send('Hello from Express! ' + logMessage)
-})
+app.use('/alexa' , router);
 
 app.server.listen(process.env.PORT || config.port || PORT, () => {
     console.log(`Started on port ${app.server.address().port}`);
---
+++ openhab-alexa/src/routes.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { log } from 'util';
+import express from 'express'
+const router = express.Router();
+
+let httpClient = axios.create({
+    headers: {"Content-Type": "text/plain",  "Accept": "application/json"}
+})
+
+router.get('/', (request, response) => {
+    response.send('Hello GET from Express!')
+})
+
+router.post('/', (request, response) => {
+    console.log(request.body.amount)
+    var alexaRequest = request.body
+    var dimmerPercentage = 0
+    /*if (alexaRequest.amount === "ON") {
+        dimmerPercentage = 100;
+    } else if (alexaRequest.amount === "OFF") {
+        dimmerPercentage = 0;
+    } else {
+        */
+        dimmerPercentage = alexaRequest.amount;
+    //}
+    const openhabReq = {
+        url: "http://openhab:8080/rest/items/Kitchen_Light_Dimmer",
+        //url: "http://openhab:8080" + "/rest/items/" + alexaRequest.item,
+        payload: dimmerPercentage
+    }
+
+    const logMessage = "Sending HTTP GET with body '"+openhabReq.payload+"' to " + openhabReq.url
+    httpClient.post(openhabReq.url, openhabReq.payload)
+         .then(response => console.log(response))
+         .catch(err => console.log(err));
+    response.send('Hello from Express! ' + logMessage)
+})
+
+export default router
